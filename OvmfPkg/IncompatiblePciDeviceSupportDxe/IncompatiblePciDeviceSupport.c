b2e20c86fbf0b3310e1e2ec8bb2a3e401c42cb2a
Peter Fang <peter.fang@intel.com>
2023-02-17 16:00:01 -0800

    OvmfPkg/IncompatiblePciDeviceSupportDxe: Ignore OptionRom
    TD-partitioned guests are unable to create page aliases in the PCI MMIO
    space, rendering OptionRom to be inaccessible to the guest. Skip
    OptionRom processing by applying the logic in
    c477b2783f6d95b62266f4fff9b0c286ac1d7bb8 to all guest types.
    
    This needs to be reworked later.
    
    Signed-off-by: Peter Fang <peter.fang@intel.com>

--- OvmfPkg/IncompatiblePciDeviceSupportDxe/IncompatiblePciDeviceSupport.c
+++ OvmfPkg/IncompatiblePciDeviceSupportDxe/IncompatiblePciDeviceSupport.c
@@ -205,7 +205,7 @@ CheckDevice (
   //
   // In Td guest OptionRom is not allowed.
   //
-  if (CcProbe ()) {
+  if (CcProbe () || TRUE) {
     Length += sizeof mOptionRomConfiguration;
   }
 
@@ -227,7 +227,7 @@ CheckDevice (
   CopyMem (Ptr, &mMmio64Configuration, sizeof mMmio64Configuration);
   Length = sizeof mMmio64Configuration;
 
-  if (CcProbe ()) {
+  if (CcProbe () || TRUE) {
     CopyMem (Ptr + Length, &mOptionRomConfiguration, sizeof mOptionRomConfiguration);
     Length += sizeof mOptionRomConfiguration;
   }
